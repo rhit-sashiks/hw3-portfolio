9544d96f4e893e3f1ec0933b3baaafb54bab585a
a <a>
2025-04-05 17:33:24 -0400

    fix up autonav and add a nice button to scroll to top of current fragment

--- scripts/autonav.js
+++ scripts/autonav.js
@@ -1,13 +1,18 @@
 const MIN_HEIGHT_TILL_AUTONEXT = 10
-async function scrollToNextPageIfAtEnd(currentY, isEnd) {
-    // Wait 500 ms to make sure the user can at least see whats in the footer
-    // Citation: https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
-    await new Promise(resolve => setTimeout(resolve, 500))
+async function scrollToNextPageIfAtEnd(isEnd) {
+    let currentY = window.scrollY
 
-    console.log(currentY)
+    let scrollToTopBtn = document.querySelector("#scrolltotop-btn")
+    if (currentY > 30) {
+        scrollToTopBtn.classList.add("scrolltotop-btn-visible")
+    } else {
+        scrollToTopBtn.classList.remove("scrolltotop-btn-visible")
+    }
+
+    currentY = window.scrollY
+
     if (currentY == 0) {
         let path = window.location.pathname.split("?")[0]
-        console.log(path)
 
         if (path.endsWith("portfolio.html")) {
             window.location.href = "./about.html"
@@ -17,38 +22,58 @@ async function scrollToNextPageIfAtEnd(currentY, isEnd) {
         return
     }
 
-    if (!isEnd) {
-        return // only proceed if the user has *finished* scrolling to improve user engagement
+    if (isEnd) {
+        return // only proceed if the user is still scrolling to improve user engagement
     }
 
     /*CITATION: https://developer.mozilla.org/en-US/docs/Web/API/Window/innerHeight and https://developer.mozilla.org/en-US/docs/Web/API/Element/clientHeight for starting resources. Some experimentation in the browser devtools then brought me to scrollHeight and visualViewport.height (i still dont know what visualviewport is but the browser devtools says it exists and it seems to work for the purpose of determining end of page) */
     let pageHeight = document.querySelector("html").scrollHeight - visualViewport.height
-    console.log(pageHeight, currentY, pageHeight - currentY)
+    console.log("pageHeight -> ", pageHeight, currentY, pageHeight - currentY)
 
-    if (pageHeight - currentY > MIN_HEIGHT_TILL_AUTONEXT) {
-        return
-    }
+    if (pageHeight - currentY <= 3) {
+        let path = window.location.pathname.split("?")[0]
 
-    let path = window.location.pathname.split("?")[0]
-    console.log(path)
-
-    if (path.endsWith("about.html")) {
-        window.location.href = "./portfolio.html"
-    } else if (path.endsWith("portfolio.html")) {
-        // Do nothing
-    } else {
-        window.location.href = "./about.html"
+        if (path.endsWith("about.html")) {
+            window.location.href = "./portfolio.html"
+        } else if (path.endsWith("portfolio.html")) {
+            // Do nothing
+        } else {
+            window.location.href = "./about.html"
+        }
     }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     let tickingA = false;
+    let tickingB = false;
+
+    let scrollToTopBtn = document.createElement("button")
+    scrollToTopBtn.id = "scrolltotop-btn"
+    scrollToTopBtn.textContent = "^"
+    scrollToTopBtn.addEventListener("click", async (evt) => {
+        tickingA = true
+        tickingB = true
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+
+        // Wait till scroll finishes
+        while (window.scrollY != 0) {
+            // Citation: https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
+            await new Promise(resolve => setTimeout(resolve, 100))
+            continue
+        }
+        tickingA = false
+        tickingB = false
+    })
+    document.body.appendChild(scrollToTopBtn)
+
     window.addEventListener("scroll", (evt) => {
         // CITATION: https://developer.mozilla.org/en-US/docs/Web/API/Document/scroll_event
-        let lastKnownScrollPosition = window.scrollY
         if (!tickingA) {
             window.requestAnimationFrame(() => {
-                scrollToNextPageIfAtEnd(lastKnownScrollPosition, false)
+                scrollToNextPageIfAtEnd(false)
                 tickingA = false;
             })
         }
@@ -56,13 +81,11 @@ document.addEventListener("DOMContentLoaded", () => {
         tickingA = true;
     })
 
-    let tickingB = false;
     window.addEventListener("scrollend", (evt) => {
         // CITATION: https://developer.mozilla.org/en-US/docs/Web/API/Document/scroll_event
-        let lastKnownScrollPosition = window.scrollY
         if (!tickingB) {
             window.requestAnimationFrame(() => {
-                scrollToNextPageIfAtEnd(lastKnownScrollPosition, true)
+                scrollToNextPageIfAtEnd(true)
                 tickingB = false;
             })
         }
